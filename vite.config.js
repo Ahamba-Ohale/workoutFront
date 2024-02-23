43ad3099e1697e855ca3e928f000023fc3101e5e
Ohale Ahamba <mg.ekezie@gmail.com>
2024-02-23 13:57:09 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    outDir: '../backend/public', // Output build files to the backend's public directory
+    assetsDir: '.',
+  },
   server: {
     port: 8080, 
   },
