c546479268cc535a36634fa21bddf25aa53989f7
Ohale Ahamba <mg.ekezie@gmail.com>
2024-02-24 14:23:40 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  "scripts": {
-    "build": "vite build"
+  build: {
+    target: 'esnext',
+    outDir: 'build', // Adjust this to your desired output directory
   },
   plugins: [react()],
 })
